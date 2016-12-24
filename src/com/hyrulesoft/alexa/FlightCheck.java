37f3c6cf51bcd96224cf33bba6a0db61f1759da3
Ashwin Swaroop <Ashwin Swaroop>
2016-12-23 20:20:55 -0800

    Fixed bugs and made code changes for first release.

--- src/com/hyrulesoft/alexa/FlightCheck.java
+++ src/com/hyrulesoft/alexa/FlightCheck.java
@@ -26,137 +26,192 @@ import com.amazon.speech.speechlet.SpeechletResponse;
 import com.amazon.speech.ui.PlainTextOutputSpeech;
 import com.amazon.speech.ui.Reprompt;
 import com.amazon.speech.ui.SimpleCard;
+import com.amazonaws.util.json.JSONArray;
+import com.amazonaws.util.json.JSONObject;
 
 public class FlightCheck implements Speechlet {
-private static final Logger log = LoggerFactory.getLogger(FlightCheck.class);
+	private static final Logger log = LoggerFactory.getLogger(FlightCheck.class);
 
-/**
- * Array containing space facts.
- */
+	/**
+	 * Array containing space facts.
+	 */
 
-@Override
-public void onSessionStarted(final SessionStartedRequest request, final Session session)
-        throws SpeechletException {
-    log.info("onSessionStarted requestId={}, sessionId={}", request.getRequestId(),
-            session.getSessionId());
-}
+	@Override
+	public void onSessionStarted(final SessionStartedRequest request, final Session session) throws SpeechletException {
+		log.info("onSessionStarted requestId={}, sessionId={}", request.getRequestId(), session.getSessionId());
+	}
 
-@Override
-public SpeechletResponse onLaunch(final LaunchRequest request, final Session session)
-        throws SpeechletException {
-    log.info("onLaunch requestId={}, sessionId={}", request.getRequestId(),
-            session.getSessionId());
-    return getHelpResponse();
-}
+	@Override
+	public SpeechletResponse onLaunch(final LaunchRequest request, final Session session) throws SpeechletException {
+		log.info("onLaunch requestId={}, sessionId={}", request.getRequestId(), session.getSessionId());
+		return getHelpResponse();
+	}
 
-@Override
-public SpeechletResponse onIntent(final IntentRequest request, final Session session)
-        throws SpeechletException {
-    log.info("onIntent requestId={}, sessionId={}", request.getRequestId(),
-            session.getSessionId());
+	@Override
+	public SpeechletResponse onIntent(final IntentRequest request, final Session session) throws SpeechletException {
+		log.info("onIntent requestId={}, sessionId={}", request.getRequestId(), session.getSessionId());
 
-    Intent intent = request.getIntent();
-    String intentName = (intent != null) ? intent.getName() : null;
-    Slot Airline = null;
-    Slot FlightNum = null;
-    Slot Destination = null;
+		Intent intent = request.getIntent();
+		String intentName = (intent != null) ? intent.getName() : null;
+		Slot Airline = null;
+		Slot FlightNum = null;
+		Slot Destination = null;
 
-    if ("CheckFlight".equals(intentName)) {
-    	
-    	Airline = intent.getSlot("Airline");
-    	FlightNum = intent.getSlot("FlightNum");
-    	Destination = intent.getSlot("Destination");
-        try {
-			return getNewCheckFlightResponse(Airline,FlightNum,Destination);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (com.amazonaws.util.json.JSONException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		if ("CheckFlight".equals(intentName)) {
+
+			Airline = intent.getSlot("Airline");
+			FlightNum = intent.getSlot("FlightNum");
+			Destination = intent.getSlot("Destination");
+			log.info("FlightNum = " + FlightNum.getValue());
+
+			try {
+				return getNewCheckFlightResponse(Airline, FlightNum, Destination);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (com.amazonaws.util.json.JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			throw new SpeechletException("Invalid Flight Number");
+
+		} else {
+			throw new SpeechletException("Invalid Intent");
 		}
-        throw new SpeechletException("Invalid Flight Number");
+	}
 
-    } else {
-        throw new SpeechletException("Invalid Intent");
-    }
+	@Override
+	public void onSessionEnded(final SessionEndedRequest request, final Session session) throws SpeechletException {
+		log.info("onSessionEnded requestId={}, sessionId={}", request.getRequestId(), session.getSessionId());
+		// any cleanup logic goes here
+	}
+
+	/**
+	 * Gets a random new fact from the list and returns to the user.
+	 * 
+	 * @throws com.amazonaws.util.json.JSONException
+	 * @throws IOException
+	 */
+	private SpeechletResponse getNewCheckFlightResponse(Slot Airline, Slot FlightNum, Slot Destination)
+			throws IOException, com.amazonaws.util.json.JSONException {
+
+		String arrivalTime = getArrivalTime(Airline.getValue(), FlightNum.getValue());
+
+		String speechText = "Your flight is scheduled to arrive at " + arrivalTime + " local time";
+
+		// Create the Simple card content.
+		SimpleCard card = new SimpleCard();
+		card.setTitle("FlightCheck");
+		card.setContent(speechText);
+
+		// Create the plain text output.
+		PlainTextOutputSpeech speech = new PlainTextOutputSpeech();
+		speech.setText(speechText);
+
+		return SpeechletResponse.newTellResponse(speech, card);
+	}
+
+	/**
+	 * Returns a response for the help intent.
+	 */
+	private SpeechletResponse getHelpResponse() {
+		String speechText = "For example, you can ask Flight Check what is the status of Lufthansa flight 112";
+		// Create the plain text output.
+		PlainTextOutputSpeech speech = new PlainTextOutputSpeech();
+		speech.setText(speechText);
+
+		// Create reprompt
+		Reprompt reprompt = new Reprompt();
+		reprompt.setOutputSpeech(speech);
+
+		return SpeechletResponse.newAskResponse(speech, reprompt);
+	}
+
+	public String getArrivalTime(String Airline, String flightNum)
+			throws IOException, com.amazonaws.util.json.JSONException {
+
+		String airlineCode = null;
+		URL url1 = new URL("https://iatacodes.org/api/v6/airlines?api_key=9661f1fe-2d46-490c-b475-161c41dd432d");
+		URLConnection uc = url1.openConnection();
+		uc.connect();
+		StringBuilder JSON_DATA = new StringBuilder();
+		BufferedReader br = new BufferedReader(new InputStreamReader(uc.getInputStream()));
+		String l = null;
+		while ((l = br.readLine()) != null) {
+			JSON_DATA = JSON_DATA.append(l);
+		}
+		// br.close();
+		System.out.println(JSON_DATA);
+		com.amazonaws.util.json.JSONObject obj = new com.amazonaws.util.json.JSONObject(JSON_DATA.toString());
+		JSONArray response = obj.getJSONArray("response");
+		for (int i = 0; i < response.length(); i++) {
+			JSONObject airline = response.getJSONObject(i);
+			if (airline.getString("name").equalsIgnoreCase(Airline)) {
+				airlineCode = airline.getString("code");
+				System.out.println(airlineCode);
+			}
+
+		}
+
+		String userPassword = "swarooprao:baf818d30d41d7670790c4112afd40501b4329fd";
+		String encoding = new String(Base64.encodeBase64(userPassword.getBytes()));
+
+		URL url0 = new URL(
+				"http://flightxml.flightaware.com/json/FlightXML2/InFlightInfo?ident=" + airlineCode + flightNum);
+		uc = url0.openConnection();
+		uc.setRequestProperty("Authorization", "Basic " + encoding);
+		uc.connect();
+		JSON_DATA = new StringBuilder();
+		br = new BufferedReader(new InputStreamReader(uc.getInputStream()));
+		l = null;
+		while ((l = br.readLine()) != null) {
+			JSON_DATA = JSON_DATA.append(l);
+		}
+		// br.close();
+		System.out.println(JSON_DATA);
+		obj = new com.amazonaws.util.json.JSONObject(JSON_DATA.toString());
+		String faFlightID = obj.getJSONObject("InFlightInfoResult").getString("faFlightID");
+		String airportID = obj.getJSONObject("InFlightInfoResult").getString("destination");
+
+		URL url2 = new URL("http://flightxml.flightaware.com/json/FlightXML2/AirportInfo?airportCode=" + airportID);
+		uc = url2.openConnection();
+		uc.setRequestProperty("Authorization", "Basic " + encoding);
+		uc.connect();
+		JSON_DATA = new StringBuilder();
+		br = new BufferedReader(new InputStreamReader(uc.getInputStream()));
+		l = null;
+		while ((l = br.readLine()) != null) {
+			JSON_DATA = JSON_DATA.append(l);
+		}
+		// 1482572033
+		System.out.println(JSON_DATA);
+		obj = new com.amazonaws.util.json.JSONObject(JSON_DATA.toString());
+		String timezone = obj.getJSONObject("AirportInfoResult").getString("timezone");
+		System.out.println(timezone);
+
+		URL url = new URL("http://flightxml.flightaware.com/json/FlightXML2/FlightInfoEx?ident=" + faFlightID);
+		uc = url.openConnection();
+		uc.setRequestProperty("Authorization", "Basic " + encoding);
+		uc.connect();
+		JSON_DATA = new StringBuilder();
+		br = new BufferedReader(new InputStreamReader(uc.getInputStream()));
+		l = null;
+		while ((l = br.readLine()) != null) {
+			JSON_DATA = JSON_DATA.append(l);
+		}
+		br.close();
+		// 1482572033
+		System.out.println(JSON_DATA);
+		obj = new com.amazonaws.util.json.JSONObject(JSON_DATA.toString());
+		String a = obj.getJSONObject("FlightInfoExResult").getJSONArray("flights").getJSONObject(0)
+				.getString("estimatedarrivaltime");
+		System.out.println(a);
+		Date date = new Date(Long.parseLong(a) * 1000);
+		// SimpleDateFormat formatter = new SimpleDateFormat("MM/dd/yyyy");
+
+		SimpleDateFormat formatter = new SimpleDateFormat("hh:mm a");
+		formatter.setTimeZone(TimeZone.getTimeZone(timezone));
+		String time = formatter.format(date);
+		return time;
+	}
 }
-
-@Override
-public void onSessionEnded(final SessionEndedRequest request, final Session session)
-        throws SpeechletException {
-    log.info("onSessionEnded requestId={}, sessionId={}", request.getRequestId(),
-            session.getSessionId());
-    // any cleanup logic goes here
-}
-
-/**
- * Gets a random new fact from the list and returns to the user.
- * @throws com.amazonaws.util.json.JSONException 
- * @throws IOException 
- */
-private SpeechletResponse getNewCheckFlightResponse(Slot Airline, Slot FlightNum, Slot Destination) throws IOException, com.amazonaws.util.json.JSONException {
-
-	String arrivalTime = getArrivalTime(FlightNum.getValue());
-	
-    String speechText = "Flight "+FlightNum.getValue()+" is scheduled to arrive at "+arrivalTime;
-
-    // Create the Simple card content.
-    SimpleCard card = new SimpleCard();
-    card.setTitle("FlightCheck");
-    card.setContent(speechText);
-
-    // Create the plain text output.
-    PlainTextOutputSpeech speech = new PlainTextOutputSpeech();
-    speech.setText(speechText);
-
-    return SpeechletResponse.newTellResponse(speech, card);
-}
-
-/**
- * Returns a response for the help intent.
- */
-private SpeechletResponse getHelpResponse() {
-    String speechText =
-            "For example, you can ask Flight Check what is the status of Lufthansa flight 112";
-    // Create the plain text output.
-    PlainTextOutputSpeech speech = new PlainTextOutputSpeech();
-    speech.setText(speechText);
-
-    // Create reprompt
-    Reprompt reprompt = new Reprompt();
-    reprompt.setOutputSpeech(speech);
-
-    return SpeechletResponse.newAskResponse(speech, reprompt);
-}
-
-public String getArrivalTime(String flightNum) throws IOException, com.amazonaws.util.json.JSONException {
-	
-	URL url = new URL("http://flightxml.flightaware.com/json/FlightXML2/FlightInfo?ident="+flightNum+"&howMany=1");
-	String userPassword = "swarooprao:baf818d30d41d7670790c4112afd40501b4329fd";
-	String encoding = new String(Base64.encodeBase64(userPassword.getBytes()));
-	URLConnection uc = url.openConnection();
-	uc.setRequestProperty("Authorization", "Basic " + encoding);
-	uc.connect();
-	StringBuilder JSON_DATA = new StringBuilder();
-	BufferedReader br = new BufferedReader(new InputStreamReader(uc
-            .getInputStream()));
-        String l = null;
-        while ((l=br.readLine())!=null) {
-            JSON_DATA = JSON_DATA.append(l);
-        }
-        br.close();
-        System.out.println(JSON_DATA);
-        com.amazonaws.util.json.JSONObject obj = new com.amazonaws.util.json.JSONObject(JSON_DATA.toString());
-        String a = obj.getJSONObject("FlightInfoResult").getJSONArray("flights").getJSONObject(0).getString("estimatedarrivaltime");
-        System.out.println(a);
-        Date date = new Date(Long.parseLong(a));
-        SimpleDateFormat formatter = new SimpleDateFormat("MM/dd/yyyy");
-
-        formatter = new SimpleDateFormat("hh:mm a");
-        formatter.setTimeZone(TimeZone.getTimeZone("America/Detroit"));;
-        String time = formatter.format(date);
-        return time;
-}
-}
-
--- src/com/hyrulesoft/alexa/FlightCheckSpeechletRequestStreamHandler.java
+++ src/com/hyrulesoft/alexa/FlightCheckSpeechletRequestStreamHandler.java
@@ -15,25 +15,27 @@ import java.util.Set;
 import com.amazon.speech.speechlet.lambda.SpeechletRequestStreamHandler;
 
 /**
- * This class could be the handler for an AWS Lambda function powering an Alexa Skills Kit
- * experience. To do this, simply set the handler field in the AWS Lambda console to
- * "spacegeek.SpaceGeekSpeechletRequestStreamHandler" For this to work, you'll also need to build
- * this project using the {@code lambda-compile} Ant task and upload the resulting zip file to power
+ * This class could be the handler for an AWS Lambda function powering an Alexa
+ * Skills Kit experience. To do this, simply set the handler field in the AWS
+ * Lambda console to "spacegeek.SpaceGeekSpeechletRequestStreamHandler" For this
+ * to work, you'll also need to build this project using the
+ * {@code lambda-compile} Ant task and upload the resulting zip file to power
  * your function.
  */
 public final class FlightCheckSpeechletRequestStreamHandler extends SpeechletRequestStreamHandler {
-    private static final Set<String> supportedApplicationIds;
+	private static final Set<String> supportedApplicationIds;
 
-    static {
-        /*
-         * This Id can be found on https://developer.amazon.com/edw/home.html#/ "Edit" the relevant
-         * Alexa Skill and put the relevant Application Ids in this Set.
-         */
-        supportedApplicationIds = new HashSet<String>();
-        // supportedApplicationIds.add("amzn1.echo-sdk-ams.app.[unique-value-here]");
-    }
+	static {
+		/*
+		 * This Id can be found on https://developer.amazon.com/edw/home.html#/
+		 * "Edit" the relevant Alexa Skill and put the relevant Application Ids
+		 * in this Set.
+		 */
+		supportedApplicationIds = new HashSet<String>();
+		// supportedApplicationIds.add("amzn1.echo-sdk-ams.app.[unique-value-here]");
+	}
 
-    public FlightCheckSpeechletRequestStreamHandler() {
-        super(new FlightCheck(), supportedApplicationIds);
-    }
+	public FlightCheckSpeechletRequestStreamHandler() {
+		super(new FlightCheck(), supportedApplicationIds);
+	}
 }
